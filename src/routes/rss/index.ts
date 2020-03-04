5ef8b76c3d24e303c85ae5d816d0ad19044a7ecf
김우재 <wj1187@naver.com>
2020-03-04 11:54:15 -0500

    fix: replace ltgt in rss

--- src/routes/rss/index.ts
+++ src/routes/rss/index.ts
@@ -22,29 +22,43 @@ router.get(
     recentPosts.forEach((post): void => {
       const paragraphs = post.content
         .split('\n')
-        .map((v): string => `<p>${v}</p>`)
+        .map((v): string => `<p>${replaceLtGt(v)}</p>`)
       let content = ''
       paragraphs.forEach((p): void => {
         content += p
       })
+      const publishedDate = new Date(post.createdAt)
       feed.item({
         title: post.title || '',
         url: `https://bamboo.dimigo.dev/post/${post.number}`,
         description: post.tag,
-        date: new Date(post.createdAt),
+        date: publishedDate,
         guid: post.id.toString(),
         custom_elements: [
           { link: `https://bamboo.dimigo.dev/post/${post.number}` },
           {
-            'content:encoded': `<!doctype html><html><head><meta charset="utf-8"></head><body><article><header><h1>#${
-              post.number
-            }번째코드</h1>${
-              post.title ? `<h2>${replaceLtGt(post.title)}</h2>` : ''
-            }<h3 class="op-kicker">${
-              post.tag
-            }</h3><figure><img src="https://i.postimg.cc/wBJRKDty/bamboocover.jpg" /></figure></header>${replaceLtGt(
-              content
-            )}</article></body></html>`
+            'content:encoded': `
+            <!doctype html>
+            <html>
+              <head>
+                <meta charset="utf-8">
+                <meta property="op:version" content="1.0.1" />
+              </head>
+              <body>
+                <article>
+                  <header>
+                    <h1>#${post.number}번째코드</h1>
+                    ${post.title ? `<h2>${replaceLtGt(post.title)}</h2>` : ''}
+                    <h3 class="op-kicker">${post.tag}</h3>
+                    <figure><img src="https://i.postimg.cc/wBJRKDty/bamboocover.jpg" /></figure>
+                    <time class="op-published" datetime=${publishedDate.toISOString()}>
+                      ${timeText(publishedDate)}
+                    </time>
+                  </header>
+                  ${content}
+                </article>
+              </body>
+            </html>`
           }
         ]
       })
@@ -57,6 +71,19 @@ router.get(
 
 const replaceLtGt = (text: string): string => {
   return text.replace('<', '&lt;').replace('>', '&gt;')
+}
+
+const timeText = (date: Date): string => {
+  let hours = date.getUTCHours() + 9
+  if (hours > 23) hours -= 24
+  let text = `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDate()}일 `
+  if (hours < 6) text += '새벽'
+  else if (hours < 11) text += '아침'
+  else if (hours < 14) text += '점심'
+  else if (hours < 15) text += '오후'
+  else if (hours < 20) text += '저녁'
+  else if (hours < 24) text += '밤'
+  return text
 }
 
 export default router
